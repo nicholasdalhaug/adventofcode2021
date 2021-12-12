8c9f0e53d6f14325e4794ccf3ab1f50926c49b80
Nicholas Dalhaug <nicholasdalhaug@gmail.com>
2021-12-12 12:17:04 +0100

    11.1

---
+++ 11/main.py
@@ -0,0 +1,99 @@
+import numpy as np
+
+class EnergyMatrix:
+    def __init__(self, energy_ints_matrix: list[list[int]]) -> None:
+        self.energy_ints_matrix = energy_ints_matrix
+        self.n_flashes = 0
+
+    def step(self):
+        for y in range(len(self.energy_ints_matrix)):
+            for x in range(len(self.energy_ints_matrix[0])):
+                self.energy_ints_matrix[y][x] += 1
+        
+        self.flash_matrix()
+
+        self.set_flashed_to_0()
+    
+    def flash_matrix(self):
+        cells_to_flash = []
+        for y in range(len(self.energy_ints_matrix)):
+            for x in range(len(self.energy_ints_matrix[0])):
+                value = self.energy_ints_matrix[y][x]
+                if value == 10:
+                    cells_to_flash.append((x, y))
+        
+        for cell in cells_to_flash:
+            xc, yc = cell
+            self.flash_cell(xc, yc)
+    
+    def flash_cell(self, x, y):
+        self.n_flashes += 1
+
+        adjacent_cells = self.get_adjacent_cells(x, y)
+        for cell in adjacent_cells:
+            xc, yc = cell
+
+            self.energy_ints_matrix[yc][xc] += 1
+            if self.energy_ints_matrix[yc][xc] == 10:
+                self.flash_cell(xc, yc)
+
+    def get_adjacent_cells(self, x, y):
+        delta_cells = np.array([
+            np.array([-1, -1]),
+            np.array([-1, 0]),
+            np.array([-1, 1]),
+            np.array([0, -1]),
+            np.array([0, 1]),
+            np.array([1, -1]),
+            np.array([1, 0]),
+            np.array([1, 1])
+        ])
+
+        cell = np.array([x, y])
+
+        cells = delta_cells + cell
+
+        adjacent_cells = [(xc, yc) for xc, yc in cells if 0 <= xc < len(self.energy_ints_matrix[0]) and 0 <= yc < len(self.energy_ints_matrix)]
+
+        return adjacent_cells
+    
+    def set_flashed_to_0(self):
+        for y in range(len(self.energy_ints_matrix)):
+            for x in range(len(self.energy_ints_matrix[0])):
+                value = self.energy_ints_matrix[y][x]
+                if value >= 10:
+                    self.energy_ints_matrix[y][x] = 0
+
+def main_from_input(content: str):
+    lines = [x.strip() for x in content.strip().split("\n")]
+    energy_ints_matrix = [[int(x) for x in line] for line in lines]
+
+    energy_matrix = EnergyMatrix(energy_ints_matrix)
+
+    for _ in range(100):
+        energy_matrix.step()
+    
+    print(energy_matrix.n_flashes)
+    return energy_matrix.n_flashes
+
+def main():
+    with open("11/input.txt") as file:
+        content = file.read()
+    
+    main_from_input(content)
+
+assert main_from_input("""
+5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526
+""") == 1656
+
+if __name__ == "__main__":
+    main()
