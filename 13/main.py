8b18467dbac4336e3fa1ef9b92b2e2032fd4ef86
Nicholas Dalhaug <nicholasdalhaug@gmail.com>
2021-12-13 17:31:17 +0100

    13.2

--- 13/main.py
+++ 13/main.py
@@ -101,7 +101,7 @@ def main_from_input(content: str):
 
         
         map.print()
-        break
+        #break
     
     score = map.count_dots()
     print(score)
@@ -113,29 +113,29 @@ def main():
     
     main_from_input(content)
 
-assert main_from_input("""
-6,10
-0,14
-9,10
-0,3
-10,4
-4,11
-6,0
-6,12
-4,1
-0,13
-10,12
-3,4
-3,0
-8,4
-1,10
-2,14
-8,10
-9,0
+# assert main_from_input("""
+# 6,10
+# 0,14
+# 9,10
+# 0,3
+# 10,4
+# 4,11
+# 6,0
+# 6,12
+# 4,1
+# 0,13
+# 10,12
+# 3,4
+# 3,0
+# 8,4
+# 1,10
+# 2,14
+# 8,10
+# 9,0
 
-fold along y=7
-fold along x=5
-""") == 17
+# fold along y=7
+# fold along x=5
+# """) == 17
 
 if __name__ == "__main__":
     main()
