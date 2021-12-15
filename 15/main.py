0da4c34554e29e562687d2c2f6d2100c0f7c9e2a
Nicholas Dalhaug <nicd@equinor.com>
2021-12-15 07:53:54 +0100

    15.1

---
+++ 15/main.py
@@ -0,0 +1,61 @@
+def main_from_input(content: str):
+    map_matrix = [[int(x) for x in line.strip()] for line in content.strip().split("\n")]
+
+    path = get_shortest_path(map_matrix, (0, 0))
+
+    end = (len(map_matrix[0])-1, len(map_matrix)-1)
+    risk = path[end][1]
+
+    print(risk)
+    return risk
+
+def get_shortest_path(map_matrix, start_coord):
+    visited_nodes = {start_coord: (None, 0)}
+    start_neighbours = get_neighbours(start_coord, map_matrix)
+    nodes_to_visit = {n: (start_coord, map_matrix[n[1]][n[0]]) for n in start_neighbours}
+
+    while len(nodes_to_visit) != 0:
+        lowest_cost_node = sorted(nodes_to_visit.items(), key= lambda n: n[1][1])[0][0]
+        
+        visited_from, cost = nodes_to_visit.pop(lowest_cost_node)
+
+        visited_nodes[lowest_cost_node] = (visited_from, cost)
+
+        neighbours = get_neighbours(lowest_cost_node, map_matrix)
+        for n in neighbours:
+            cost_to_n = cost + map_matrix[n[1]][n[0]]
+            if n not in visited_nodes and (n not in nodes_to_visit or nodes_to_visit[n][1] > cost_to_n):
+                nodes_to_visit[n] = (lowest_cost_node, cost_to_n)
+
+    return visited_nodes
+
+def get_neighbours(coord, map_matrix):
+    x, y = coord
+    neighbours = []
+    if x > 0: neighbours.append((x-1, y))
+    if x < len(map_matrix[0])-1: neighbours.append((x+1, y))
+    if y > 0: neighbours.append((x, y-1))
+    if y < len(map_matrix)-1: neighbours.append((x, y+1))
+    return neighbours
+
+def main():
+    with open("15/input.txt") as file:
+        content = file.read()
+    
+    main_from_input(content)
+
+assert main_from_input("""
+1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581
+""") == 40
+
+if __name__ == "__main__":
+    main()
